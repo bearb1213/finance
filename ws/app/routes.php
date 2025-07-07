e897252b70fd82d4b6dbe03950930c3a2f3ed442
bearb1213 <bearb1145@gmail.com>
2025-07-07 17:34:37 +0600

    com

--- ws/app/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-$s = DIRECTORY_SEPARATOR;
-
-require_once 'vendor'.$s.'autoload.php';
-
-
-
-require_once 'app'.$s.'routes'.$s.'EtudiantRoute.php';
--- ws/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-require_once 'vendor/autoload.php';
-require 'app/routes.php';
-
-
-Flight::start();
